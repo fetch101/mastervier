4b98745ae0e75e7264dc226d36a170b565a0f5fd
fetch101 <pascalvetsch@hotmail.com>
2015-03-29 14:43:26 +0200

    refacter circle view

--- Views/CircleView.cs
+++ Views/CircleView.cs
@@ -14,68 +14,69 @@ public class CircleView : MonoBehaviour {
 	
 	}
 
-
-    //TODO refactor this
-    //TODO call this when similarities change
+ 
+    //TODO update this if similarities change
     public void alignContents(Content center)
     {
         SimKeeper simKeeper = getSimKeeper();
         List<KeyValuePair<Content, int>> simList = simKeeper.getSimListForContent(center);
-        float currRad = 60;
-        Vector3 currPos = new Vector3(0f, 0f, 0f);
-        center.transform.position = currPos;
-        int sameScore = 1;
-        //TODO content with no similarities has an empty simList
+        float currRadius = 0;
+        center.transform.position = new Vector3(0f, 0f, 0f);
 
-
-        int currScore = simList[0].Value;
-        int circlePos = 1;
-
-        while (simList[sameScore].Value == currScore)
+        while (simList.Count > 0)
         {
-            sameScore++;
-        }
-        //TODO make sure all elements have enough space on currRad
+            int currValue = simList[0].Value;
+            List<Content> currContentCircle = getAndRemoveAllContentsWithValue(simList, currValue);
+            currRadius = setNewRadius(currRadius, currContentCircle);
+            Circle circle = new Circle(currRadius, currContentCircle.Count);
 
-
-        Circle circle = new Circle(currRad, sameScore);
-
-        for (int i = 0; i < simList.Count; i++)
-        {
-            currPos = circle.getPosForElement(circlePos);
-            simList[i].Key.transform.position = currPos;
-            circlePos++;
-
-            if (i + 1 < simList.Count && simList[i + 1].Value != currScore)
+            for (int i = 0; i < currContentCircle.Count; i++ )
             {
-                sameScore = 1;
-                currScore = simList[i + 1].Value;
-                while (i + sameScore + 1 < simList.Count && simList[i + sameScore + 1].Value == currScore)
-                {
-                    sameScore++;
-                }
-
-                if (getMinRadius(sameScore) > currRad + 60)
-                {
-                    currRad = getMinRadius(sameScore);
-                }
-                else
-                {
-                    currRad += 60;
-                }
-
-                circle = new Circle(currRad, sameScore);
-                circlePos = 1;
-
+                currContentCircle[i].transform.position = circle.getPosForElement(i);
             }
-
         }
 
+              
+    }
+
+    private float setNewRadius(float currRadius, List<Content> currContentCircle)
+    {
+        if (getMinRadius(currContentCircle.Count) > currRadius)
+        {
+            currRadius = getMinRadius(currContentCircle.Count);
+        }
+        else
+        {
+            currRadius += 60;
+        }
+        return currRadius;
     }
 
     private float getMinRadius(int numberOfElements)
     {
-        return (numberOfElements * 60) / (2 * Mathf.PI);
+        float minRad = (numberOfElements * 60) / (2 * Mathf.PI);
+        return minRad >= 60 ? minRad : 60;
+    }
+
+    private List<Content> getAndRemoveAllContentsWithValue(List<KeyValuePair<Content, int>> simList, int value)
+    {
+        List<Content> sameValueList = new List<Content>();
+        
+        foreach (KeyValuePair<Content, int> pair in simList)
+        {
+            if (pair.Value >= value)
+            {
+                sameValueList.Add(pair.Key);
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        simList.RemoveAll(item => item.Value.Equals(value));
+        
+        return sameValueList;
     }
 
     private SimKeeper getSimKeeper()
