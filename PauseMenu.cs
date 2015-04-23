6c499094d853c8364739cba80fd6a4071a111b2e
parallelix <itz@DesignMac-109.local>
2015-04-23 20:39:10 +0200

    started UI Text Display programming

--- PauseMenu.cs
+++ PauseMenu.cs
@@ -7,7 +7,7 @@ using UnityEngine.UI;
 
 
 public class PauseMenu : MonoBehaviour {
-
+	
 	public List<KeyValuePair<ButtonForTag, String>> buttonForTagWithTagList;
 
     private List<KeyValuePair<String, String>> stringList = new List<KeyValuePair<String, String>>();
@@ -71,12 +71,12 @@ public class PauseMenu : MonoBehaviour {
 
 	Content objectToDisplayTags;
 
-	public GameObject TagButton0;
-	public GameObject TagButton1;
-	public GameObject TagButton2;
-	public GameObject TagButton3;
-	public GameObject TagButton4;
-	public GameObject TagButton5;
+	public GameObject TagButton0_0;
+	public GameObject TagButton0_1;
+	public GameObject TagButton0_2;
+	public GameObject TagButton0_3;
+	public GameObject TagButton0_4;
+	public GameObject TagButton0_5;
 	public GameObject TagButton6;
 	public GameObject TagButton7;
 	public GameObject TagButton8;
@@ -94,25 +94,109 @@ public class PauseMenu : MonoBehaviour {
 	public GameObject TagButton20;
 	public GameObject TagButton21;
 
+	public GameObject TagValue_0;
+	public GameObject TagValue_1;
+	public GameObject TagValue_2;
+	public GameObject TagValue_3;
+	public GameObject TagValue_4;
+	public GameObject TagValue_5;
+	public GameObject TagValue_6;
+	public GameObject TagValue_7;
+	public GameObject TagValue_8;
+	public GameObject TagValue_9;
+	public GameObject TagValue_10;
+	public GameObject TagValue_11;
+	public GameObject TagValue_12;
+	public GameObject TagValue_13;
+	public GameObject TagValue_14;
+	public GameObject TagValue_15;
+	public GameObject TagValue_16;
+	public GameObject TagValue_17;
+	public GameObject TagValue_18;
+	public GameObject TagValue_19;
+	public GameObject TagValue_20;
+	public GameObject TagValue_21;
 
+	public GameObject TagKey_0;
+	public GameObject TagKey_1;
+	public GameObject TagKey_2;
+	public GameObject TagKey_3;
 
+	
 
+	// nur um auszuprobieren:
+	Text TagValue_0Text;
+	Text TagValue_1Text;
+	Text TagValue_2Text;
+	Text TagValue_3Text;
+	Text TagValue_4Text;
+	Text TagValue_5Text;
+	Text TagValue_6Text;
+	Text TagValue_7Text;
+	Text TagValue_8Text;
+	Text TagValue_9Text;
+	Text TagValue_10Text;
+	Text TagValue_11Text;
+	Text TagValue_12Text;
+	Text TagValue_13Text;
+	Text TagValue_14Text;
+	Text TagValue_15Text;
+	Text TagValue_16Text;
+	Text TagValue_17Text;
+	Text TagValue_18Text;
+	Text TagValue_19Text;
+	Text TagValue_20Text;
+	Text TagValue_21Text;
 
+	Text TagKey_0Text;
+	Text TagKey_1Text;
+	Text TagKey_2Text;
+	Text TagKey_3Text;
 
-
+	
 	public GameObject PauseMenuObject;
-
+	public GameObject DisplayTagsRuntimeCanvas;
     private bool isPause;
 
     // Use this for initialization
     void Start()
     {
-		TagButton0 = GameObject.Find ("TagButton0");
-		TagButton1 = GameObject.Find ("TagButton1");
-		TagButton2 = GameObject.Find ("TagButton2");
-		TagButton3 = GameObject.Find ("TagButton3");
-		TagButton4 = GameObject.Find ("TagButton4");
-		TagButton5 = GameObject.Find ("TagButton5");
+
+		TagValue_0 = GameObject.Find ("TagValue_0");
+		TagValue_1 = GameObject.Find ("TagValue_1");
+		TagValue_2 = GameObject.Find ("TagValue_2");
+		TagValue_3 = GameObject.Find ("TagValue_3");
+		TagValue_4 = GameObject.Find ("TagValue_4");
+		TagValue_5 = GameObject.Find ("TagValue_5");
+		TagValue_6 = GameObject.Find ("TagValue_6");
+		TagValue_7 = GameObject.Find ("TagValue_7");
+		TagValue_8 = GameObject.Find ("TagValue_8");
+		TagValue_9 = GameObject.Find ("TagValue_9");
+		TagValue_10 = GameObject.Find ("TagValue_10");
+		TagValue_11 = GameObject.Find ("TagValue_11");
+		TagValue_12 = GameObject.Find ("TagValue_12");
+		TagValue_13 = GameObject.Find ("TagValue_13");
+		TagValue_14 = GameObject.Find ("TagValue_14");
+		TagValue_15 = GameObject.Find ("TagValue_15");
+		TagValue_16 = GameObject.Find ("TagValue_16");
+		TagValue_17 = GameObject.Find ("TagValue_17");
+		TagValue_18 = GameObject.Find ("TagValue_18");
+		TagValue_19 = GameObject.Find ("TagValue_19");
+		TagValue_20 = GameObject.Find ("TagValue_20");
+		TagValue_21 = GameObject.Find ("TagValue_21");
+
+		TagKey_0 = GameObject.Find ("TagKey_0");
+		TagKey_1 = GameObject.Find ("TagKey_1");
+		TagKey_2 = GameObject.Find ("TagKey_2");
+		TagKey_3 = GameObject.Find ("TagKey_3");
+
+		
+		TagButton0_0 = GameObject.Find ("TagButton0_0");
+		TagButton0_1 = GameObject.Find ("TagButton0_1");
+		TagButton0_2 = GameObject.Find ("TagButton0_2");
+		TagButton0_3 = GameObject.Find ("TagButton0_3");
+		TagButton0_4 = GameObject.Find ("TagButton0_4");
+		TagButton0_5 = GameObject.Find ("TagButton0_5");
 		TagButton6= GameObject.Find ("TagButton6");
 		TagButton7 = GameObject.Find ("TagButton7");
 		TagButton8 = GameObject.Find ("TagButton8");
@@ -130,6 +214,8 @@ public class PauseMenu : MonoBehaviour {
 		TagButton20 = GameObject.Find ("TagButton20");
 		TagButton21 = GameObject.Find ("TagButton21");
 		PauseMenuObject = GameObject.Find ("PauseMenuObject");
+		DisplayTagsRuntimeCanvas = GameObject.Find ("DisplayTagsRuntimeCanvas");
+		DisplayTagsRuntimeCanvas.SetActive (false);
         Cursor.lockState = CursorLockMode.Confined;
         Cursor.visible = true;
         isPause = true;
@@ -142,12 +228,14 @@ public class PauseMenu : MonoBehaviour {
 
 		isInSight = CheckRay ();
 
-        if (Input.GetKeyDown(KeyCode.Escape))
+		showTagsAtRuntime();
+
+		
+		if (Input.GetKeyDown(KeyCode.Escape))
         {
             togglePause();
-			ShowPauseMenu();
-//			getButtonForTagWithTagList();
-        }
+
+		}
 
     }
 
@@ -179,18 +267,18 @@ public class PauseMenu : MonoBehaviour {
 			KeyValuePair<String, String> curTag = new KeyValuePair<String, String>("Student", contentInSight.Student);
 			stringList.Add(curTag);
 		}
-		if (contentInSight.Semester != "") {
-			KeyValuePair<String, String> curTag = new KeyValuePair<String, String>("Semester", contentInSight.Semester);
-			stringList.Add(curTag);
-		}
-		if (contentInSight.Phase != "") {
-			KeyValuePair<String, String> curTag = new KeyValuePair<String, String>("Phase", contentInSight.Phase);
-			stringList.Add(curTag);
-		}
-		if (contentInSight.Year != "") {
-			KeyValuePair<String, String> curTag = new KeyValuePair<String, String>("Jahr", contentInSight.Year);
-			stringList.Add(curTag);
-		}
+//		if (contentInSight.Semester != "") {
+//			KeyValuePair<String, String> curTag = new KeyValuePair<String, String>("Semester", contentInSight.Semester);
+//			stringList.Add(curTag);
+//		}
+//		if (contentInSight.Phase != "") {
+//			KeyValuePair<String, String> curTag = new KeyValuePair<String, String>("Phase", contentInSight.Phase);
+//			stringList.Add(curTag);
+//		}
+//		if (contentInSight.Year != "") {
+//			KeyValuePair<String, String> curTag = new KeyValuePair<String, String>("Jahr", contentInSight.Year);
+//			stringList.Add(curTag);
+//		}
 		if (contentInSight.Objecttype != "") {
 			KeyValuePair<String, String> curTag = new KeyValuePair<String, String>("Objekttyp", contentInSight.Objecttype);
 			stringList.Add(curTag);
@@ -261,178 +349,185 @@ public class PauseMenu : MonoBehaviour {
 		}
 
 		return stringList;
-		
+
 	}
 
-
-
-
-    void OnGUI()
-    {
-        if (isPause)
-        {
-
-            GUILayout.Label("Game is paused!");
-            if (GUILayout.Button("Line View"))
-                ViewKeeper.instance.lineView();
-            if (GUILayout.Button("Cube View"))
-                ViewKeeper.instance.cubeView();
-            if (GUILayout.Button("Sun View"))
-                ViewKeeper.instance.sunView();
-            if (GUILayout.Button("TEST Wordcloud"))
-                ViewKeeper.instance.wordCloud();
-//            if (GUILayout.Button("TEST Highlight DarioSala"))
-//                Highlighter.instance.highlightContentFromStudent("DarioSala");
-//            if (GUILayout.Button("TEST Highlight AliceGut"))
-////                Highlighter.instance.highlightContentFromStudent("AliceGut");
-            if (GUILayout.Button("TEST Remove Highlight"))
-                Highlighter.instance.removeCurrentHighlight();     
-
-				}
-
-	
-
-
-
-		if (isInSight == true && isPause) {
-			
-			
-			GUI.TextField (new Rect (810, 220, ((6 * textFieldWidth) + 110), numberOfDots), "");
-			numberOfDots = (stringList.Count * 20) + 5;
-			
-			int offsetCounter = 10;
-			foreach (KeyValuePair<String, String> tag in stringList) {
-				
-				GUI.Label (new Rect (815, 210 + offsetCounter, 300, 300), tag.Key);
-				GUI.Label (new Rect (915, 210 + offsetCounter, 3000, 300), tag.Value);
-
-				if (tag.Value.Length > textFieldWidth) {
-					textFieldWidth = tag.Value.Length;
-				}
-				
-				offsetCounter += 20;
-			}
-		} else {
-			textFieldWidth = 0;
-				
-		}
-}
-
-
-	void ShowPauseMenu (){
-		
-		if (isPause) {
-			PauseMenuObject.SetActive (true);
-
-			if (isInSight){
-
-
-			if (stringList.Count >= 0) {
-				TagButton0.SetActive (true);
-			}
-			if (stringList.Count >= 1) {
-				TagButton1.SetActive (true);
-			}
-			if (stringList.Count >= 2) {
-				TagButton2.SetActive (true);
-			}
-			if (stringList.Count >= 3) {
-				TagButton3.SetActive (true);
-			}
-			if (stringList.Count >= 4) {
-				TagButton4.SetActive (true);
-			}
-			if (stringList.Count >= 5) {
-				TagButton5.SetActive (true);
-			}
-			if (stringList.Count >= 6) {
-				TagButton6.SetActive (true);
-			}
-			if (stringList.Count >= 7) {
-				TagButton7.SetActive (true);
-			}
-			if (stringList.Count >= 8) {
-				TagButton8.SetActive (true);
-			}
-			if (stringList.Count >= 9) {
-				TagButton9.SetActive (true);
-			}
-			if (stringList.Count >= 10) {
-				TagButton10.SetActive (true);
-			}
-			if (stringList.Count >= 11) {
-				TagButton11.SetActive (true);
-			}
-			if (stringList.Count >= 12) {
-				TagButton12.SetActive (true);
-			}
-			if (stringList.Count >= 13) {
-				TagButton13.SetActive (true);
-			}
-			if (stringList.Count >= 14) {
-				TagButton14.SetActive (true);
-			}
-			if (stringList.Count >= 15) {
-				TagButton15.SetActive (true);
-			}
-			if (stringList.Count >= 16) {
-				TagButton16.SetActive (true);
-			}
-			if (stringList.Count >= 17) {
-				TagButton17.SetActive (true);
-			}
-			if (stringList.Count >= 18) {
-				TagButton18.SetActive (true);
-			}
-			if (stringList.Count >= 19) {
-				TagButton19.SetActive (true);
-			}
-			if (stringList.Count >= 20) {
-				TagButton20.SetActive (true);
-			}
-			if (stringList.Count >= 21) {
-				TagButton21.SetActive (true);
-			}
-			}
-
-		} else {
-				PauseMenuObject.SetActive (false);
-			}
-	}
-
-
-
-
-	
-//	public List<KeyValuePair<ButtonForTag, string>> getButtonForTagWithTagList(){
-//		
-//		UnityEngine.Object[] objectButtons = FindObjectsOfType(typeof(ButtonForTag));
-//		Debug.Log ("objectbuttons: " + objectButtons.Length);
-//		
-//		List<ButtonForTag> buttonList = new List<ButtonForTag>();
-//		foreach (ButtonForTag btn in objectButtons)
-//		{
-//			buttonList.Add((ButtonForTag)btn);
-//		}
-//		
-//		buttonForTagWithTagList = new List<KeyValuePair<ButtonForTag, String>>();
-//		Debug.Log ("stringlistcount: " + stringList.Count);
-//		Debug.Log ("buttonlistcount: " + buttonList.Count);
-//		
-//		for (int i = 0; i < stringList.Count; i++)
-//		{
-//			string tag = stringList[i].Value;
-//			ButtonForTag button = buttonList[i];
+//	public List<TagValueTextObject> tagValueTextObjectList (){
 //
-//			buttonForTagWithTagList.Add(new KeyValuePair<ButtonForTag, string>(button, tag));
+//
+//
+//		UnityEngine.Object[] tagValueTextObjects = FindObjectsOfType(typeof(TagValueTextObject));
+//		
+//		List<TagValueTextObject> tagValueTextObjectList = new List<TagValueTextObject>();
+//
+//
+//		return tagValueTextObjectList;
+//		
+//	}
+	
+	
+//	void OnGUI()
+//	{
+//		if (isPause) {
+//
+//			GUILayout.Label ("Game is paused!");
+//			if (GUILayout.Button ("Line View"))
+//				ViewKeeper.instance.lineView ();
+//			if (GUILayout.Button ("Cube View"))
+//				ViewKeeper.instance.cubeView ();
+//			if (GUILayout.Button ("Sun View"))
+//				ViewKeeper.instance.sunView ();
+//			if (GUILayout.Button ("TEST Wordcloud"))
+//				ViewKeeper.instance.wordCloud ();
+////            if (GUILayout.Button("TEST Highlight DarioSala"))
+////                Highlighter.instance.highlightContentFromStudent("DarioSala");
+////            if (GUILayout.Button("TEST Highlight AliceGut"))
+//////                Highlighter.instance.highlightContentFromStudent("AliceGut");
+//			if (GUILayout.Button ("TEST Remove Highlight"))
+//				Highlighter.instance.removeCurrentHighlight ();     
 //
 //		}
-//		return buttonForTagWithTagList;
-//		Debug.Log ("buttonfortagwithtaglis: " + buttonForTagWithTagList.Count);
-//
 //	}
 
 
+
+
+
+	void showTagsAtRuntime(){
+
+//		UnityEngine.Object[] tagValueTextObjects = FindObjectsOfType(typeof(TagValueTextObject));
+//		List<TagValueTextObject> tagValueTextObjectList = new List<TagValueTextObject>();
+//		UnityEngine.Object[] tagKeyTextObjects = FindObjectsOfType(typeof(TagKeyTextObject));
+//		List<TagKeyTextObject> tagKeyTextObjectList = new List<TagKeyTextObject>();
+//		if (isInSight == true) {
+//		int i;
+//		for (i= 0; i < stringList.Count; i++){
+//
+//			foreach(GameObject TagKeyTextObject in tagKeyTextObjectList)
+//			{
+//				gameObject = tagKeyTextObjectList[i];
+//				gameObject.SetActive(true);
+//				i++;
+//					Debug.Log("I have createt a Button with the name" + gameObject );
+//			}
+//		}
+//		for (int j= 0; j < stringList.Count; j++){
+//			
+//			foreach(GameObject TagValueTextObject in tagValueTextObjectList)
+//			{
+//				GameObject = tagValueTextObjectList[i];
+//				GameObject.SetActive(true);
+//				j++;
+//					Debug.Log("I have createt a flutton with the name" + gameObject );
+//			}
+//		}
+//	}
+
+		// TODO: Only do this if Content in sight has changed!!!
+
+		if (isInSight == true) {
+
+
+			DisplayTagsRuntimeCanvas.SetActive (true);
+			int i = 0;
+			int j = 0;
+
+
+				while (stringList.Count >= i && i == j) {
+					if(stringList [i].Value != ""){
+					TagValue_0.SetActive (true);
+					TagKey_0.SetActive(true);
+					TagValue_0Text = TagValue_0.GetComponent<Text> ();
+					TagValue_0Text.text = stringList [i].Value;
+					TagKey_0Text = TagKey_0.GetComponent<Text> ();
+					TagKey_0Text.text = stringList [i].Key;
+					j++;
+					} else { 
+						i++;
+						j++;
+					}
+				}
+				i = j;
+
+				while (stringList.Count >= i && i == j) {
+					if(stringList [i].Value != ""){
+					TagValue_1.SetActive (true);
+					TagKey_1.SetActive(true);
+					TagValue_1Text = TagValue_1.GetComponent<Text> ();
+					TagValue_1Text.text = stringList [i].Value;
+					TagKey_1Text = TagKey_1.GetComponent<Text> ();
+					TagKey_1Text.text = stringList [i].Key;
+					j++;
+					} else { 
+						i++;
+						j++;
+					}
+				}
+				i = j;
+				while (stringList.Count >= i && i == j) {
+					if(stringList [i].Value != ""){
+					TagValue_2.SetActive (true);
+					TagKey_2.SetActive(true);
+					TagValue_2Text = TagValue_2.GetComponent<Text> ();
+					TagValue_2Text.text = stringList [i].Value;
+					TagKey_2Text = TagKey_2.GetComponent<Text> ();
+					TagKey_2Text.text = stringList [i].Key;
+					j++;
+					} else { 
+						i++;
+						j++;
+					}
+				}
+				i = j;
+
+
+
+			}
+
+	}
+
+//		else {
+//			DisplayTagsRuntimeCanvas.SetActive(false);
+//			TagValue_0.SetActive(false);
+//			TagValue_1.SetActive(false);
+//			TagValue_2.SetActive(false);
+//			TagValue_3.SetActive(false);
+//			TagValue_4.SetActive(false);
+//			TagValue_5.SetActive(false);
+//			TagValue_6.SetActive(false);
+//			TagValue_7.SetActive(false);
+//			TagValue_8.SetActive(false);
+//			TagValue_9.SetActive(false);
+//			TagValue_10.SetActive(false);
+//
+//		}
+//	}
+
+		
+			
+
+	//NOED LOESCHE WEGE DE BERECHNIG
+
+//		if (isInSight == true && isPause) {
+//			
+//			
+//			GUI.TextField (new Rect (810, 220, ((6 * textFieldWidth) + 110), numberOfDots), "");
+//			numberOfDots = (stringList.Count * 20) + 5;
+//			
+//			int offsetCounter = 10;
+//			foreach (KeyValuePair<String, String> tag in stringList) {
+//				
+//				GUI.Label (new Rect (815, 210 + offsetCounter, 300, 300), tag.Key);
+//				GUI.Label (new Rect (915, 210 + offsetCounter, 3000, 300), tag.Value);
+//
+//				if (tag.Value.Length > textFieldWidth) {
+//					textFieldWidth = tag.Value.Length;
+//				}
+//				
+//				offsetCounter += 20;
+//			}
+//		} else {
+		//			textFieldWidth = }
 
     void togglePause()
     {
@@ -454,30 +549,32 @@ public class PauseMenu : MonoBehaviour {
         ContentManager.instance.enabled = true;
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+		PauseMenuObject.SetActive (false);
 
-
-		TagButton0.SetActive(false);
-		TagButton1.SetActive(false);
-		TagButton2.SetActive(false);
-		TagButton3.SetActive(false);
-		TagButton4.SetActive(false);
-		TagButton5.SetActive(false);
-		TagButton6.SetActive(false);
-		TagButton7.SetActive(false);
-		TagButton8.SetActive(false);
-		TagButton9.SetActive(false);
-		TagButton10.SetActive(false);
-		TagButton11.SetActive(false);
-		TagButton12.SetActive(false);
-		TagButton13.SetActive(false);
-		TagButton14.SetActive(false);
-		TagButton15.SetActive(false);
-		TagButton16.SetActive(false);
-		TagButton17.SetActive(false);
-		TagButton18.SetActive(false);
-		TagButton19.SetActive(false);
-		TagButton20.SetActive(false);
-		TagButton21.SetActive(false);
+		
+		//
+//		TagButton0.SetActive(false);
+//		TagButton1.SetActive(false);
+//		TagButton2.SetActive(false);
+//		TagButton3.SetActive(false);
+//		TagButton4.SetActive(false);
+//		TagButton5.SetActive(false);
+//		TagButton6.SetActive(false);
+//		TagButton7.SetActive(false);
+//		TagButton8.SetActive(false);
+//		TagButton9.SetActive(false);
+//		TagButton10.SetActive(false);
+//		TagButton11.SetActive(false);
+//		TagButton12.SetActive(false);
+//		TagButton13.SetActive(false);
+//		TagButton14.SetActive(false);
+//		TagButton15.SetActive(false);
+//		TagButton16.SetActive(false);
+//		TagButton17.SetActive(false);
+//		TagButton18.SetActive(false);
+//		TagButton19.SetActive(false);
+//		TagButton20.SetActive(false);
+//		TagButton21.SetActive(false);
 
 		stringList.Clear();
 
@@ -494,8 +591,9 @@ public class PauseMenu : MonoBehaviour {
         ContentManager.instance.enabled = false;
 
         PauseMenuObject.SetActive(true);
+		DisplayTagsRuntimeCanvas.SetActive (true);
 
-        Cursor.lockState = CursorLockMode.Confined;
+		Cursor.lockState = CursorLockMode.Confined;
         Cursor.visible = true;
         isPause = true;
     }
