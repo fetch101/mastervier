299e6d476b0b86cbeebb7c8a1afe482434eeca7d
fetch101 <pascalvetsch@hotmail.com>
2015-05-07 19:55:34 +0200

    display all tags, count per student

--- DisplayAllTags.cs
+++ DisplayAllTags.cs
@@ -16,12 +16,12 @@ public class DisplayAllTags : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-//        if (i == 100)
-//        {
-//            tagDic = SimKeeper.instance.getTagDic();
-//            printTags();
-//        }
-//        i++;
+        if (i == 100)
+        {
+            tagDic = SimKeeper.instance.getTagDic();
+            printTags();
+        }
+        i++;
 	}
 
     private void printTags()
@@ -38,8 +38,8 @@ public class DisplayAllTags : MonoBehaviour {
             string outputstring = "";
             foreach (KeyValuePair<string, int> pair in studentDic)
             {
-//                outputstring += pair.Value;
-//                outputstring += " - ";
+                outputstring += pair.Value;
+                outputstring += " - ";
                 outputstring += pair.Key;
                 outputstring += "\n";
             }
@@ -51,143 +51,107 @@ public class DisplayAllTags : MonoBehaviour {
     {
         List<string> tagList = content.getTagList();
 
-//		if (content.Autor != "")
-//		{
-//			studentDic.Add(content.Autor, tagDic[content.Autor]);
-//		}
-//		
-//		if (content.Verortung != "")
-//		{
-//			studentDic.Add(content.Verortung, tagDic[content.Verortung]);
-//		}
-//		
-//		if (content.UR != "" )
-//		{
-//			studentDic.Add(content.UR, tagDic[content.UR]);
-//		}
-//		
-//		if (content.PUR != "" )
-//		{
-//			studentDic.Add(content.PUR, tagDic[content.PUR]);
-//		}
-//		if (content.Tag0 != "" )
-//		{
-//			studentDic.Add(content.Tag0, tagDic[content.Tag0]);
-//		}
-//		if (content.Tag1 != "" )
-//		{
-//			studentDic.Add(content.Tag1, tagDic[content.Tag1]);
-//		}
-//		if (content.Tag2 != "" )
-//		{
-//			studentDic.Add(content.Tag2, tagDic[content.Tag2]);
-//		}
-//		if (content.Tag3 != "" )
-//		{
-//			studentDic.Add(content.Tag3, tagDic[content.Tag3]);
-//		}
-//		if (content.Tag4 != "" )
-//		{
-//			studentDic.Add(content.Tag4, tagDic[content.Tag4]);
-//		}
-//		if (content.Tag5 != "" )
-//		{
-//			studentDic.Add(content.Tag5, tagDic[content.Tag5]);
-//		}
-//		if (content.Tag6 != "" )
-//		{
-//			studentDic.Add(content.Tag6, tagDic[content.Tag6]);
-//		}
-//		if (content.Tag7 != "" )
-//		{
-//			studentDic.Add(content.Tag7, tagDic[content.Tag7]);
-//		}
-//		if (content.Tag8 != "" )
-//		{
-//			studentDic.Add(content.Tag8, tagDic[content.Tag8]);
-//		}
-//		if (content.Tag9 != "" )
-//		{
-//			studentDic.Add(content.Tag9, tagDic[content.Tag9]);
-//		}
 
-
-
-
-
-
-
-
-
-
-
-        if (content.Autor != "" && !studentDic.ContainsKey(content.Autor))
+        if (content.Autor != "")
         {
-            studentDic.Add(content.Autor, tagDic[content.Autor]);
+            if (!studentDic.ContainsKey(content.Autor))
+            {
+                    studentDic.Add(content.Autor, 1);
+            }
+            else
+            {
+                studentDic[content.Autor]++;
+            }
+        }
+        if (content.Verortung != "")
+        {
+            if (!studentDic.ContainsKey(content.Verortung))
+            {
+                studentDic.Add(content.Verortung, 1);
+            }
+            else
+            {
+                studentDic[content.Verortung]++;
+            }
         }
 
-        if (content.Verortung != "" && !studentDic.ContainsKey(content.Verortung))
+        if (content.UR != "")
         {
-            studentDic.Add(content.Verortung, tagDic[content.Verortung]);
+            if (!studentDic.ContainsKey(content.UR))
+            {
+                studentDic.Add(content.UR, 1);
+            }
+            else
+            {
+                studentDic[content.UR]++;
+            }
         }
 
-        if (content.UR != "" && !studentDic.ContainsKey(content.UR))
+        if (content.PUR != "")
         {
-            studentDic.Add(content.UR, tagDic[content.UR]);
+            if (!studentDic.ContainsKey(content.PUR))
+            {
+                studentDic.Add(content.PUR, 1);
+            }
+            else
+            {
+                studentDic[content.PUR]++;
+            }
         }
 
-        if (content.PUR != "" && !studentDic.ContainsKey(content.PUR))
-        {
-            studentDic.Add(content.PUR, tagDic[content.PUR]);
-        }
-		if (content.Tag0 != "" && !studentDic.ContainsKey(content.Tag0))
-		{
-			studentDic.Add(content.Tag0, tagDic[content.Tag0]);
-		}
-		if (content.Tag1 != "" && !studentDic.ContainsKey(content.Tag1))
-		{
-			studentDic.Add(content.Tag1, tagDic[content.Tag1]);
-		}
-		if (content.Tag2 != "" && !studentDic.ContainsKey(content.Tag2))
-		{
-			studentDic.Add(content.Tag2, tagDic[content.Tag2]);
-		}
-		if (content.Tag3 != "" && !studentDic.ContainsKey(content.Tag3))
-		{
-			studentDic.Add(content.Tag3, tagDic[content.Tag3]);
-		}
-		if (content.Tag4 != "" && !studentDic.ContainsKey(content.Tag4))
-		{
-			studentDic.Add(content.Tag4, tagDic[content.Tag4]);
-		}
-		if (content.Tag5 != "" && !studentDic.ContainsKey(content.Tag5))
-		{
-			studentDic.Add(content.Tag5, tagDic[content.Tag5]);
-		}
-		if (content.Tag6 != "" && !studentDic.ContainsKey(content.Tag6))
-		{
-			studentDic.Add(content.Tag6, tagDic[content.Tag6]);
-		}
-		if (content.Tag7 != "" && !studentDic.ContainsKey(content.Tag7))
-		{
-			studentDic.Add(content.Tag7, tagDic[content.Tag7]);
-		}
-		if (content.Tag8 != "" && !studentDic.ContainsKey(content.Tag8))
-		{
-			studentDic.Add(content.Tag8, tagDic[content.Tag8]);
-		}
-		if (content.Tag9 != "" && !studentDic.ContainsKey(content.Tag9))
-		{
-			studentDic.Add(content.Tag9, tagDic[content.Tag9]);
-		}
 
         foreach (string tag in tagList)
         {
-            if (!tagDic.ContainsKey(tag))
+            if (!studentDic.ContainsKey(tag))
             {
-                tagDic.Add(tag, tagDic[tag]);
+                studentDic.Add(tag, 1);
+            }
+            else
+            {
+                studentDic[tag]++;
             }
         }
+
+        //if (content.Tag0 != "" && !studentDic.ContainsKey(content.Tag0))
+        //{
+        //    studentDic.Add(content.Tag0, tagDic[content.Tag0]);
+        //}
+        //if (content.Tag1 != "" && !studentDic.ContainsKey(content.Tag1))
+        //{
+        //    studentDic.Add(content.Tag1, tagDic[content.Tag1]);
+        //}
+        //if (content.Tag2 != "" && !studentDic.ContainsKey(content.Tag2))
+        //{
+        //    studentDic.Add(content.Tag2, tagDic[content.Tag2]);
+        //}
+        //if (content.Tag3 != "" && !studentDic.ContainsKey(content.Tag3))
+        //{
+        //    studentDic.Add(content.Tag3, tagDic[content.Tag3]);
+        //}
+        //if (content.Tag4 != "" && !studentDic.ContainsKey(content.Tag4))
+        //{
+        //    studentDic.Add(content.Tag4, tagDic[content.Tag4]);
+        //}
+        //if (content.Tag5 != "" && !studentDic.ContainsKey(content.Tag5))
+        //{
+        //    studentDic.Add(content.Tag5, tagDic[content.Tag5]);
+        //}
+        //if (content.Tag6 != "" && !studentDic.ContainsKey(content.Tag6))
+        //{
+        //    studentDic.Add(content.Tag6, tagDic[content.Tag6]);
+        //}
+        //if (content.Tag7 != "" && !studentDic.ContainsKey(content.Tag7))
+        //{
+        //    studentDic.Add(content.Tag7, tagDic[content.Tag7]);
+        //}
+        //if (content.Tag8 != "" && !studentDic.ContainsKey(content.Tag8))
+        //{
+        //    studentDic.Add(content.Tag8, tagDic[content.Tag8]);
+        //}
+        //if (content.Tag9 != "" && !studentDic.ContainsKey(content.Tag9))
+        //{
+        //    studentDic.Add(content.Tag9, tagDic[content.Tag9]);
+        //}
     }
 
     private List<List<Content>> getStudentList()
