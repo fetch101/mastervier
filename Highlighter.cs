c1c3061f239057d9b076897bca9e1c1b5793de8a
parallelix <itz@DesignMac-109.local>
2015-05-09 16:57:07 +0200

    sync

--- Highlighter.cs
+++ Highlighter.cs
@@ -18,6 +18,11 @@ public class Highlighter : MonoBehaviour {
 	void Update () {
 	
 	}
+	// test
+	public void testsync(){
+
+		Debug.Log ("gahts no?");
+	}
 
 	public void highlightContent(string field, string value)
 	{
